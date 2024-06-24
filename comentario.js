6b86f870dcc11ccaca4c7c521bcdbb6d742db9ee
EdsonStella <eedsonstella3@gmail.com>
2024-06-24 20:12:23 -0300

    Add files via upload

---
+++ comentario.js
@@ -0,0 +1,14 @@
+// console.log é usado para exibir algo na tela do computador
+console.log('passo #01');
+console.log('passo #02'); // mais um exemplo de console.log
+// console.log('passo #03'); 
+
+/* -Sempre usar o  /* caso for fazar um comentario de varias
+linhas, e no final dela coloca o */
+
+/* Esse é um 
+   comentario de 
+   múltiplas 
+   linhas.
+*/   
+console.log("passo #3")
---
+++ dados.js
@@ -0,0 +1,17 @@
+var nome = "Caneta";
+var quantidade = 10; //VAR seria um codigo de caixa que armazena itens
+var preco = 6.4;
+var imposto = 1.5;
+var precofinal = preco-imposto;
+
+console.log(nome);
+console.log(quantidade); //valor literal
+console.log(preco);
+console.log(imposto);
+console.log(precofinal);
+
+nome = "caneta BIC";
+console.log(nome);
+
+blabla = 123
+console.log(blabla)
